28298853a04bdd0c64e95dbc150149fb6086fe62
Mohammed Lateefat A <132450161+Mohiedel@users.noreply.github.com>
2023-05-04 06:55:08 +0100

    Add files via upload

--- login_dart.dart
+++ login_dart.dart
@@ -10,7 +10,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   TextEditingController emailTextEditngController = TextEditingController();
   TextEditingController passwordTextEditngController = TextEditingController();
   bool showProgressBar = false;
@@ -47,10 +46,10 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: MediaQuery.of(context).size.width,
                 margin: const EdgeInsets.symmetric(horizontal: 25),
                 child: InputTextWidget(
-                    controller: emailTextEditngController,
-                    labelString: 'Email',
-                    iconData: Icons.email_outlined,
-                    isObscure: false,
+                  controller: emailTextEditngController,
+                  labelString: 'Email',
+                  iconData: Icons.email_outlined,
+                  isObscure: false,
                 ),
               ),
               const SizedBox(
@@ -66,11 +65,67 @@ class _LoginScreenState extends State<LoginScreen> {
                   isObscure: true,
                 ),
               ),
-
-              showProgressBar == false ?
-              Column(children: Container(width: MediaQuery.of(context).size.width,),
-              )
-                  :Container(),
+              const SizedBox(
+                height: 35,
+              ),
+              showProgressBar == false
+                  ? Column(children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width - 38,
+                        height: 65,
+                        decoration: const BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(25),
+                            )),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              showProgressBar = true;
+                            });
+                          },
+                          child: const Center(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Text(
+                            'Not Registered?',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          InkWell(
+                            child: const Center(
+                              child: Text(
+                                'Sign up now',
+                                style: TextStyle(
+                                    fontSize: 28,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ])
+                  : Container(),
             ],
           ),
         ),
